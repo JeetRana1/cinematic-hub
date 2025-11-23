311a91eca5f78fb3359a10bcb44f4871874970e6
JeetRana1 <104169225+JeetRana1@users.noreply.github.com>
2025-11-23 14:22:43 -0500

    test

---
+++ public/emailConfig.js
@@ -0,0 +1,7 @@
+// This file is ignored by git. Do not commit secrets here.
+window.EMAILJS = {
+  enabled: true,
+  serviceId: 'service_g39ksvs',
+  templateId: 'template_j5mn12c',
+  publicKey: '2XLUMd-LEnSSh4rpo'
+};
---
+++ public/firebaseConfig.js
@@ -0,0 +1,9 @@
+// This file is ignored by git. Do not commit secrets here.
+window.firebaseConfig = {
+  apiKey: "AIzaSyAOwMXKzazXPvJlMI7YuRvZ0q8efCDGN3c",
+  authDomain: "auth-implemented.firebaseapp.com",
+  projectId: "auth-implemented",
+  storageBucket: "auth-implemented.firebasestorage.app",
+  messagingSenderId: "201333894254",
+  appId: "1:201333894254:web:bb7af43735d15d7aa78a7f"
+};
